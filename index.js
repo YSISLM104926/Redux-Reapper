ae30c14a5dfb8c131233ede368fc6dfda1101f61
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-20 01:36:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const {createStore} = require("redux");
+
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+
+// state 
+const initialCounterState = {
+    count: 0,
+}
+const initialUsersState = {
+    users: [{ name: 'md. likhon mia' }]
+}
+
+// action
+const incrementCounter = () => {
+    return {
+        type: INCREMENT
+    }
+}
+
+const decrementCounter = () => {
+    return {
+        type: DECREMENT
+    }
+}
+
+// Reducer
+const counterReducer = (state, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                count: state.count + 1
+            }
+        case DECREMENT:
+            return {
+                count: state.count - 1
+            }
+        default:
+            return state    
+    }
+}
+
+// store
+const store = createStore(counterReducer);
+
+
+store.subscribe(()=> {
+    console.log(store.getState());
+})
+
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
